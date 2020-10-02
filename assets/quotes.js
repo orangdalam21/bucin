6cc305987475e193347dd67f185a0adbd9f86e3f
orangdalam21 <71118752+orangdalam21@users.noreply.github.com>
2020-10-02 19:57:33 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "RxCodew", quotes: "Whatever happens, keep breathing" },
+    { author: "RedHitalubis_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Redy Ilyasa", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "RxCodew", quotes: "I love you." },
+    { author: "Redy Ilyasa", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Redy", quotes: "I love you." },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
 ];
